bdfdf53a726e214eadcca8cfe70857e3e7f7f56a
Tobias Ottenweller <mail@ottenweller.net>
2012-08-15 12:13:33 +0200

    Created 'FileMixerMeter' project and 'BA' workspace.

---
+++ code/FilePlayMixerMeter/FilePlayMixerMeter/TOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TOAppDelegate.h
+//  FilePlayMixerMeter
+//
+//  Created by Tobias Ottenweller on 15.08.12.
+//  Copyright (c) 2012 Tobias Ottenweller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ code/FilePlayMixerMeter/FilePlayMixerMeter/TOViewController.h
@@ -0,0 +1,13 @@
+//
+//  TOViewController.h
+//  FilePlayMixerMeter
+//
+//  Created by Tobias Ottenweller on 15.08.12.
+//  Copyright (c) 2012 Tobias Ottenweller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOViewController : UIViewController
+
+@end
