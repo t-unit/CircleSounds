1053f0260dbd8ae189caf87885f605e00d3ae1df
Tobias Ottenweller <mail@ottenweller.net>
2012-08-29 14:13:58 +0200

    Added CircleSounds project.

---
+++ code/CircleSounds/CircleSounds/TOViewController.h
@@ -0,0 +1,13 @@
+//
+//  TOViewController.h
+//  CircleSounds
+//
+//  Created by Tobias Ottenweller on 29.08.12.
+//  Copyright (c) 2012 Tobias Ottenweller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOViewController : UIViewController
+
+@end
---
+++ code/CircleSounds/CircleSounds/TOViewController.m
@@ -0,0 +1,29 @@
+//
+//  TOViewController.m
+//  CircleSounds
+//
+//  Created by Tobias Ottenweller on 29.08.12.
+//  Copyright (c) 2012 Tobias Ottenweller. All rights reserved.
+//
+
+#import "TOViewController.h"
+
+@interface TOViewController ()
+
+@end
+
+@implementation TOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
