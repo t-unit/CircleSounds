97cff31f1ad54093990ee659dcdfb3ddf8a42eee
Tobias Ottenweller <mail@ottenweller.net>
2012-09-07 11:31:18 +0200

    Sound Trimming Gesture Recognition.

--- code/CircleSounds/CircleSounds/TOSoundDetailsPopoverViewController.m
+++ code/CircleSounds/CircleSounds/TOSoundDetailsPopoverViewController.m
@@ -11,6 +11,7 @@
 #import "TOSoundDetailsPopoverViewControllerDelegate.h"
 #import "TOEqualizerSound.h"
 #import "TOWaveformDrawer.h"
+#import "TOHandleView.h"
 
 
 @interface TOSoundDetailsPopoverViewController ()
@@ -23,13 +24,20 @@
 {
     [super viewDidLoad];
     
-	self.loopCountLabel.text = [NSString stringWithFormat:@"%ld", self.sound.loopCount];
+    [self setViewPropertiesUsingSound];
+	[self setupGestureRecognizer];
+}
+
+
+- (void)setViewPropertiesUsingSound
+{
+    self.loopCountLabel.text = [NSString stringWithFormat:@"%ld", self.sound.loopCount];
     self.loopCountStepper.value = self.sound.loopCount;
     self.songArtistLabel.text = self.sound.fileSongArtist;
     self.songNameLabel.text = self.sound.fileSongName;
     self.songDurationLabel.text = [NSString stringWithFormat:@"%d:%d", (int)self.sound.duration/60, (int)self.sound.duration%60];
-
-
+    
+    
     for (NSUInteger i=0; i<self.sound.bands.count; i++) {
         UISlider *slider = (UISlider *)[self.equalizerCanvas viewWithTag:i];
         slider.value = [self.sound gainForBandAtPosition:i];
@@ -45,7 +53,7 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         TOWaveformDrawer *drawer = [[TOWaveformDrawer alloc] init];
         drawer.mode = TOWaveformDrawerModeRectangle;
-        drawer.waveformColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:1];
+        drawer.waveformColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
         drawer.imageSize = self.waveformImageView.bounds.size;
         
         UIImage *image = [drawer waveformFromImageAtURL:self.sound.audioFileURL];
@@ -57,13 +65,125 @@
     });
 }
 
+#pragma mark - Gesture Recognition
 
-- (void)didReceiveMemoryWarning
+- (void)setupGestureRecognizer
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleLeftHandleGesture:)];
+    [self.leftTrimmGestureCatcher addGestureRecognizer:panGestureRecognizer];
+    
+    panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleRightHandleGesture:)];
+    [self.rightTrimmGestureCatcher addGestureRecognizer:panGestureRecognizer];
 }
 
+
+- (void)handleLeftHandleGesture:(UIPanGestureRecognizer *)sender
+{
+    CGFloat translationX = [sender translationInView:self.view].x;
+
+    
+    switch (sender.state) {
+            
+        case UIGestureRecognizerStateChanged:
+        {
+            CGAffineTransform rightTransform = self.rightTrimmGestureCatcher.transform;
+            CGAffineTransform leftTransform = self.leftTrimmGestureCatcher.transform;
+            
+            // don't translate out of the super view
+            if (leftTransform.tx <= 0 && translationX < 0) {
+                self.leftTrimmGestureCatcher.transform = CGAffineTransformMakeTranslation(0, 0);
+                self.leftTrimmOverlay.transform = self.leftTrimmGestureCatcher.transform;
+                
+                break;
+            }
+            
+            // don't translate over the other trimm gesture catcher
+            if (translationX > 0 &&
+                fabsf(rightTransform.tx) + fabsf(leftTransform.tx) + self.leftTrimmGestureCatcher.frame.size.width + 4 >= self.waveformImageView.frame.size.width) {
+                
+                translationX = self.waveformImageView.frame.size.width - fabsf(rightTransform.tx) - self.leftTrimmGestureCatcher.frame.size.width - 4;
+                
+                self.leftTrimmGestureCatcher.transform = CGAffineTransformMakeTranslation(translationX, 0);
+                self.leftTrimmOverlay.transform = self.leftTrimmGestureCatcher.transform;
+                
+                break;
+            }
+            
+            // apply the translation
+            self.leftTrimmGestureCatcher.transform = CGAffineTransformTranslate(self.leftTrimmGestureCatcher.transform, translationX, 0);
+            self.leftTrimmOverlay.transform = self.leftTrimmGestureCatcher.transform;
+            break;
+        }
+            
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+        {
+            // TODO: apply the changes to the sound object
+            break;
+        }
+            
+        default:
+            break;
+    }
+    
+    [sender setTranslation:CGPointZero inView:self.view];
+}
+
+
+- (void)handleRightHandleGesture:(UIPanGestureRecognizer *)sender
+{
+    CGFloat translationX = [sender translationInView:self.view].x;
+    
+    
+    switch (sender.state) {
+            
+        case UIGestureRecognizerStateChanged:
+        {
+            CGAffineTransform rightTransform = self.rightTrimmGestureCatcher.transform;
+            CGAffineTransform leftTransform = self.leftTrimmGestureCatcher.transform;
+            
+            // don't translate out of the super view
+            if (rightTransform.tx >= 0 && translationX > 0) {
+                self.rightTrimmGestureCatcher.transform = CGAffineTransformMakeTranslation(0, 0);
+                self.rightTrimmOverlay.transform = self.rightTrimmGestureCatcher.transform;
+                
+                break;
+            }
+            
+            // don't translate over the other trimm gesture catcher
+            if (translationX < 0 &&
+                fabsf(rightTransform.tx) + fabsf(leftTransform.tx) + self.leftTrimmGestureCatcher.frame.size.width + 4 >= self.waveformImageView.frame.size.width) {
+                
+                translationX = -(self.waveformImageView.frame.size.width-fabsf(leftTransform.tx)) + self.leftTrimmGestureCatcher.frame.size.width + 4;
+                
+                self.rightTrimmGestureCatcher.transform = CGAffineTransformMakeTranslation(translationX, 0);
+                self.rightTrimmOverlay.transform = self.rightTrimmGestureCatcher.transform;
+                
+                break;
+            }
+            
+            // apply the translation
+            self.rightTrimmGestureCatcher.transform = CGAffineTransformTranslate(self.rightTrimmGestureCatcher.transform, translationX, 0);
+            self.rightTrimmOverlay.transform = self.rightTrimmGestureCatcher.transform;
+            break;
+        }
+            
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+        {
+            // TODO: apply the changes to the sound object
+            break;
+        }
+            
+        default:
+            break;
+    }
+    
+    [sender setTranslation:CGPointZero inView:self.view];
+}
+
+
+#pragma mark - Buttons Event Handling
 
 - (IBAction)loopCountStepperValueChanged:(id)sender
 {
@@ -101,6 +221,8 @@
 {
 }
 
+
+#pragma mark - EQ
 
 - (IBAction)eqSliderValueChanged:(UISlider *)sender
 {
