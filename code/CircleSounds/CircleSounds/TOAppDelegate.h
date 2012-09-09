d28c29526c86a6bfeb490f53147a256a3034999e
Tobias Ottenweller <mail@ottenweller.net>
2012-09-09 11:24:28 +0200

    Removed Methods from the App Delegate now present inside TOAudioFileManger.

--- code/CircleSounds/CircleSounds/TOAppDelegate.h
+++ code/CircleSounds/CircleSounds/TOAppDelegate.h
@@ -13,6 +13,6 @@
 @property (strong, nonatomic) UIWindow *window;
 
 
-@property (readonly, nonatomic) NSURL *recordingsDirectory;
+
 
 @end
--- code/CircleSounds/CircleSounds/TOAppDelegate.m
+++ code/CircleSounds/CircleSounds/TOAppDelegate.m
@@ -15,12 +15,14 @@
     // Override point for customization after application launch.
     return YES;
 }
-							
+
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
+
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
@@ -28,45 +30,22 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
+
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
+
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
+
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
-- (NSURL *)recordingsDirectory
-{
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    
-    NSURL *documentDirectory = [[fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-    NSURL *recodingDirectory = [documentDirectory URLByAppendingPathComponent:@"Recordings" isDirectory:YES];
-    
-    if (![fileManager fileExistsAtPath:recodingDirectory.path]) {
-        
-        NSError *error;
-        BOOL success = [fileManager createDirectoryAtURL:recodingDirectory
-                              withIntermediateDirectories:YES
-                                               attributes:nil
-                                                    error:&error];
-        
-#if DEBUG
-        if (error || !success) {
-            NSLog(@"creating recording directory failed! (%@)", error);
-        }
-#endif
-    }
-    
-    return recodingDirectory;
-}
-
 
 @end
