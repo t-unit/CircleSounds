a4900970be570beded3cb989033e3fa78fbe98a4
Tobias Ottenweller <mail@ottenweller.net>
2012-09-11 14:00:17 +0200

    Added category NSSet+setByRemovingObject.

---
+++ code/CircleSounds/CircleSounds/NSSet+setByRemovingObject.h
@@ -0,0 +1,15 @@
+//
+//  NSSet+setByRemovingObject.h
+//  CircleSounds
+//
+//  Created by Tobias Ottenweller on 11.09.12.
+//  Copyright (c) 2012 Tobias Ottenweller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSSet (setByRemovingObject)
+
+- (NSSet *)setByRemovingObject:(id)anObject;
+
+@end
