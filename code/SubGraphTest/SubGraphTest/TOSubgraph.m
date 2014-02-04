5baadf5f5abe06c8ef4ceab31f67fcc7ea18afbd
Tobias Ottenweller <mail@ottenweller.net>
2014-02-04 16:14:38 +0100

    Updated the sub graph test project.

--- code/SubGraphTest/SubGraphTest/TOSubgraph.m
+++ code/SubGraphTest/SubGraphTest/TOSubgraph.m
@@ -170,7 +170,7 @@
 
 - (void)initializePlayerUnit
 {
-    NSURL *songURL = [[NSBundle mainBundle] URLForResource:@"guitarStereo" withExtension:@"caf"];
+    NSURL *songURL = [[NSBundle mainBundle] URLForResource:@"pump_im" withExtension:@"mp3"];
     TOThrowOnError(AudioFileOpenURL((__bridge CFURLRef)(songURL), kAudioFileReadPermission, 0, &audioFile));
     
     TOThrowOnError(AudioUnitSetProperty(filePlayerUnit,
